92cccbd7c688363dd214f6aba521d6190adc3208
Rajeev kumar <105671691+Raj-eevKumar@users.noreply.github.com>
2024-05-04 19:56:21 +0530

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,49 @@
+// <!---->: html
+// /*  */ : CSS
+// JS: Single line comment: //
+// JS: Multiline comment: /*  */
+
+
+//Creating variable
+
+// declaration a variable with some name: creating variable
+// var pictures 
+// var fruits
+// var sarika
+// var swati
+
+//pictures="chintu photos" // Initialization: putting some data to the variable
+
+//used for printing the variable value
+//console.log(pictures)
+
+
+//Initialization at the time declaration:
+
+//var x = 4
+
+//console.log(x)
+
+//Variable Naming Rules:
+
+//1. Start with underscope(_), doller, any letter(a-z, A-Z)
+
+//var x56
+
+//2. Can contain letters, numbers, underscores, and dollar signs.
+
+//var first_name
+
+//3.Case-sensitive, myVar, and myvar are considered different variables.
+
+// var hello
+// var Hello
+
+//4. Avoid using JavaScript-reserved words as variable names like.
+
+//if, else, for, let
+
+//5. Camel Case: For multi-word variable names, start with a lowercase letter and capitalize each subsequent word.
+
+// var firstName
+// var sonOfMyFather
