cfff25a18754c9d0d2c00a1554872b94bf01a791
Rajeev kumar <105671691+Raj-eevKumar@users.noreply.github.com>
2024-05-04 19:58:50 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,12 @@
+//Data Type: Number, String, Boolean
+
+var age = 10 // 
+
+console.log(typeof(age)) // Number: Any Numeric Value
+
+var name = "Ducat" // String : Set of Character
+
+var x = 'a'
+
+console.log(typeof(name))
+console.log(typeof(x))
