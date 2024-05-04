37b50eb0c61b61d918054a29824ed042a8b6c5ed
Rajeev kumar <105671691+Raj-eevKumar@users.noreply.github.com>
2024-05-04 22:45:51 +0530

    Add files via upload

---
+++ Relational Operator.js
@@ -0,0 +1,40 @@
+//Relational Operator: >(greater than), >=(greater than or equal to), <(less than), <=(less than or equal to): it is used to compare two values, It returns boolean value either true or false
+//it always uses for numerical value
+
+//Example 1:
+
+//var age = 18
+
+// creating condition for asking age greater than 18
+//var result = age > 18 // false(No)
+//var result = age >= 18 // true(Yes)
+
+//var result1 = age < 18 // false(No)
+//var result1 = age <= 18 // false(No)
+
+//console.log(result)
+//console.log(result1)
+
+
+//Example 2: put condition to check number is negative or positive
+
+//var number = -2
+
+//var result = number < 0
+//if returns false then its positive
+//if returns true than its negative
+//console.log(result)
+
+
+//Example 3: check person1 is elder than person2
+
+var personOneAge = 12
+var personTwoAge = 34
+
+var result = personOneAge > personTwoAge
+
+//if it returns true then can say person1 is elder
+//if it returns false then can say person2 is elder
+
+console.log(result)
+
