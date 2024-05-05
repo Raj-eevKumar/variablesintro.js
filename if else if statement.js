a8d867c3e0a376d30c03c136490121246248d545
Rajeev kumar <105671691+Raj-eevKumar@users.noreply.github.com>
2024-05-05 22:44:27 +0530

    class-11

---
+++ if else if statement.js
@@ -0,0 +1,32 @@
+//if else if statement: we have mutliple conditions and only the true condtion executed
+//Example of if else if else statement
+var num = 45;
+
+if(num>=50){
+   console.log("Number is greater than 50")
+}
+else if(num==45){
+    console.log("Number is equal to 45")
+}
+else if(num>=40){
+     console.log("Number is greater than 40")
+}
+else if(num>=100){
+    console.log("Number is greater than 100")
+}
+
+console.log("====================================================")
+
+// if if if statement
+
+if(num > 40){
+    console.log("Hello1")
+}
+
+if(num>=45){
+    console.log("Hello2")
+}
+
+if(num<=45){
+    console.log("Hello3")
+}
