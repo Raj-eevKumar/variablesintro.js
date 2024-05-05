67164802b894d44a531d7965df3b2d74457af161
Rajeev kumar <105671691+Raj-eevKumar@users.noreply.github.com>
2024-05-05 22:36:45 +0530

    class-8

---
+++ If-else Statement.js
@@ -0,0 +1,40 @@
+//If-else Statement: If the condition is true then code inside if block will print otherwise code inside else block.
+/*
+Syntax
+if(codintion){
+   // if the condtion is true
+}else{
+   // if the condtion is false
+}
+*/
+
+//Example 1: Basic
+
+// var chintuList="Tamatar"
+
+// if(chintuList!="Tamatar"){
+//     console.log("I have baught tamatar")
+// }else{
+//     console.log("I have baught for Aalo")
+// }
+
+//Example 2: print eligible if age >=18 else print not eligible
+
+// var age = 17
+
+// if(age >= 18){
+//     console.log("Eligible")
+// }else{
+//     console.log("Not Eligible")
+// }
+
+//Example 3:
+
+// var totalBill = 30000;
+// //let's calculate 30% discount
+// totalBill = totalBill - totalBill*30/100
+// if(totalBill <= 25000){
+//     console.log("Chhavi Father (^_^): Bill is accepted")
+// }else{
+//     console.log("Chhavi Father(~_~): Bill is not gonna accepted for next 5 years")
+// }
